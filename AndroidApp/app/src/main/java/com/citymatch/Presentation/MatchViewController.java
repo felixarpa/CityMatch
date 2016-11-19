4011e2050be2bff61a1d96261db3d50843d94676
gonmolon <gonzalo1796@gmail.com>
2016-11-19 23:03:50 +0100

    Starting match view

---
+++ AndroidApp/app/src/main/java/com/citymatch/Presentation/MatchViewController.java
@@ -0,0 +1,17 @@
+package com.citymatch.Presentation;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.citymatch.R;
+
+public class MatchViewController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.match_list_view);
+
+    }
+}
