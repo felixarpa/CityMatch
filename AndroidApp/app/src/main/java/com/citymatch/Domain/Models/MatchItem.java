07382f8e3a591da9a819efd06889357e630de9af
gonmolon <gonzalo1796@gmail.com>
2016-11-19 23:03:35 +0100

    Finishing matches list

--- AndroidApp/app/src/main/java/com/citymatch/Domain/Models/MatchItem.java
+++ AndroidApp/app/src/main/java/com/citymatch/Domain/Models/MatchItem.java
@@ -1,6 +1,6 @@
 package com.citymatch.Domain.Models;
 
-import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,15 +23,15 @@ public class MatchItem {
 
     public MatchItem() {}
 
-    public View populateListItem(Context context, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    public View populateListItem(LayoutInflater inflater, ViewGroup parent) {
         View rowView = inflater.inflate(R.layout.match_list_item, parent, false);
         TextView name = (TextView) rowView.findViewById(R.id.name);
         TextView country = (TextView) rowView.findViewById(R.id.country);
         ImageView icon = (ImageView) rowView.findViewById(R.id.icon);
         name.setText(this.name);
         country.setText(this.country);
-        //ImageLoader.getInstance().displayImage(imageURL, icon);
+        ImageLoader.getInstance().displayImage(imageURL, icon);
+
         return rowView;
     }
 }
